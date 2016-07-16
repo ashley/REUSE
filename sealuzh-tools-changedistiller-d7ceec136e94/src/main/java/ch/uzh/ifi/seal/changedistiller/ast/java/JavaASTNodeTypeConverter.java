49dc7aa4bbc176a747955d136774268fec86f91a
Claire <claire.legoues@gmail.com>
2016-07-16 17:24:30 -0400

    java astnode type converter compiles, with caveats/ todos

--- sealuzh-tools-changedistiller-d7ceec136e94/src/main/java/ch/uzh/ifi/seal/changedistiller/ast/java/JavaASTNodeTypeConverter.java
+++ sealuzh-tools-changedistiller-d7ceec136e94/src/main/java/ch/uzh/ifi/seal/changedistiller/ast/java/JavaASTNodeTypeConverter.java
@@ -75,6 +75,59 @@ import static ch.uzh.ifi.seal.changedistiller.model.classifiers.java.JavaEntityT
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ArrayAccess;
+import org.eclipse.jdt.core.dom.ArrayCreation;
+import org.eclipse.jdt.core.dom.ArrayInitializer;
+import org.eclipse.jdt.core.dom.ArrayType;
+import org.eclipse.jdt.core.dom.AssertStatement;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.BooleanLiteral;
+import org.eclipse.jdt.core.dom.BreakStatement;
+import org.eclipse.jdt.core.dom.CastExpression;
+import org.eclipse.jdt.core.dom.CharacterLiteral;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.ConditionalExpression;
+import org.eclipse.jdt.core.dom.ConstructorInvocation;
+import org.eclipse.jdt.core.dom.ContinueStatement;
+import org.eclipse.jdt.core.dom.DoStatement;
+import org.eclipse.jdt.core.dom.EmptyStatement;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.Initializer;
+import org.eclipse.jdt.core.dom.InstanceofExpression;
+import org.eclipse.jdt.core.dom.Javadoc;
+import org.eclipse.jdt.core.dom.LabeledStatement;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.NullLiteral;
+import org.eclipse.jdt.core.dom.NumberLiteral;
+import org.eclipse.jdt.core.dom.ParameterizedType;
+import org.eclipse.jdt.core.dom.PostfixExpression;
+import org.eclipse.jdt.core.dom.PrefixExpression;
+import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.QualifiedType;
+import org.eclipse.jdt.core.dom.ReturnStatement;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.SwitchCase;
+import org.eclipse.jdt.core.dom.SwitchStatement;
+import org.eclipse.jdt.core.dom.SynchronizedStatement;
+import org.eclipse.jdt.core.dom.ThrowStatement;
+import org.eclipse.jdt.core.dom.TryStatement;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.TypeLiteral;
+import org.eclipse.jdt.core.dom.TypeParameter;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+import org.eclipse.jdt.core.dom.WhileStatement;
+import org.eclipse.jdt.core.dom.WildcardType;
 
 import ch.uzh.ifi.seal.changedistiller.ast.ASTNodeTypeConverter;
 import ch.uzh.ifi.seal.changedistiller.model.classifiers.EntityType;
@@ -90,83 +143,65 @@ public final class JavaASTNodeTypeConverter implements ASTNodeTypeConverter {
 
     private static Map<Class<? extends ASTNode>, JavaEntityType> sConversionMap =
             new HashMap<Class<? extends ASTNode>, JavaEntityType>();
-
+    // FIXME: I fixed this to work with the entity types presently defined
+    // we might consider whether we should add/remove, though it looks like they
+    // are defined wrt original eclipse types, so it might be fine.
+    // FIXME: ensure there are no repetitions here, when doing above check
     static {
         sConversionMap.put(Assignment.class, ASSIGNMENT);
-        sConversionMap.put(CompoundAssignment.class, ASSIGNMENT);
         sConversionMap.put(PostfixExpression.class, POSTFIX_EXPRESSION);
         sConversionMap.put(PrefixExpression.class, PREFIX_EXPRESSION);
-        sConversionMap.put(AllocationExpression.class, CLASS_INSTANCE_CREATION);
-        sConversionMap.put(QualifiedAllocationExpression.class, CLASS_INSTANCE_CREATION);
+        sConversionMap.put(ClassInstanceCreation.class, CLASS_INSTANCE_CREATION);
         sConversionMap.put(AssertStatement.class, ASSERT_STATEMENT);
         sConversionMap.put(BreakStatement.class, BREAK_STATEMENT);
-        sConversionMap.put(ExplicitConstructorCall.class, CONSTRUCTOR_INVOCATION);
+        sConversionMap.put(ConstructorInvocation.class, CONSTRUCTOR_INVOCATION);
         sConversionMap.put(ContinueStatement.class, CONTINUE_STATEMENT);
         sConversionMap.put(DoStatement.class, DO_STATEMENT);
         sConversionMap.put(EmptyStatement.class, EMPTY_STATEMENT);
-        sConversionMap.put(ForeachStatement.class, FOREACH_STATEMENT);
+        sConversionMap.put(EnhancedForStatement.class, FOREACH_STATEMENT);
         sConversionMap.put(ForStatement.class, FOR_STATEMENT);
         sConversionMap.put(IfStatement.class, IF_STATEMENT);
         sConversionMap.put(LabeledStatement.class, LABELED_STATEMENT);
-        sConversionMap.put(LocalDeclaration.class, VARIABLE_DECLARATION_STATEMENT);
-        sConversionMap.put(MessageSend.class, METHOD_INVOCATION);
+        sConversionMap.put(VariableDeclarationStatement.class, VARIABLE_DECLARATION_STATEMENT);
+        sConversionMap.put(MethodInvocation.class, METHOD_INVOCATION);
         sConversionMap.put(ReturnStatement.class, RETURN_STATEMENT);
         sConversionMap.put(SwitchStatement.class, SWITCH_STATEMENT);
-        sConversionMap.put(CaseStatement.class, SWITCH_CASE);
-        sConversionMap.put(SingleTypeReference.class, SINGLE_TYPE); // Bug #14: Cannot distinguish between primitive and simple types without resolving bindings
+        sConversionMap.put(SwitchCase.class, SWITCH_CASE);
+        sConversionMap.put(SimpleType.class, SINGLE_TYPE); // Bug #14: Cannot distinguish between primitive and simple types without resolving bindings; FIXME: do we want to?
+        sConversionMap.put(PrimitiveType.class, SINGLE_TYPE); // Bug #14: Cannot distinguish between primitive and simple types without resolving bindings
         sConversionMap.put(SynchronizedStatement.class, SYNCHRONIZED_STATEMENT);
         sConversionMap.put(ThrowStatement.class, THROW_STATEMENT);
         sConversionMap.put(TryStatement.class, TRY_STATEMENT);
         sConversionMap.put(WhileStatement.class, WHILE_STATEMENT);
-        sConversionMap.put(ParameterizedSingleTypeReference.class, PARAMETERIZED_TYPE);
-        sConversionMap.put(ParameterizedQualifiedTypeReference.class, PARAMETERIZED_TYPE);
+        sConversionMap.put(ParameterizedType.class, PARAMETERIZED_TYPE);
         sConversionMap.put(Javadoc.class, JAVADOC);
-        sConversionMap.put(QualifiedTypeReference.class, QUALIFIED_TYPE);
-        sConversionMap.put(Argument.class, PARAMETER);
+        sConversionMap.put(QualifiedType.class, QUALIFIED_TYPE);
+       // sConversionMap.put(Argument.class, PARAMETER); FIXME: how to handle parameters?
         sConversionMap.put(TypeParameter.class, TYPE_PARAMETER);
-        sConversionMap.put(Wildcard.class, WILDCARD_TYPE);
+        sConversionMap.put(WildcardType.class, WILDCARD_TYPE);
         sConversionMap.put(StringLiteral.class, STRING_LITERAL);
-        sConversionMap.put(ExtendedStringLiteral.class, STRING_LITERAL);
-        sConversionMap.put(StringLiteralConcatenation.class, STRING_LITERAL);
-        sConversionMap.put(FalseLiteral.class, BOOLEAN_LITERAL);
-        sConversionMap.put(TrueLiteral.class, BOOLEAN_LITERAL);
+        sConversionMap.put(BooleanLiteral.class, BOOLEAN_LITERAL);
         sConversionMap.put(NullLiteral.class, NULL_LITERAL);
-        sConversionMap.put(DoubleLiteral.class, NUMBER_LITERAL);
-        sConversionMap.put(FloatLiteral.class, NUMBER_LITERAL);
-        sConversionMap.put(LongLiteral.class, NUMBER_LITERAL);
-        sConversionMap.put(LongLiteralMinValue.class, NUMBER_LITERAL);
-        sConversionMap.put(IntLiteral.class, NUMBER_LITERAL);
-        sConversionMap.put(IntLiteralMinValue.class, NUMBER_LITERAL);
-        sConversionMap.put(CharLiteral.class, CHARACTER_LITERAL);
-        sConversionMap.put(AND_AND_Expression.class, INFIX_EXPRESSION);
-        sConversionMap.put(OR_OR_Expression.class, INFIX_EXPRESSION);
-        sConversionMap.put(ArrayAllocationExpression.class, ARRAY_CREATION);
+        sConversionMap.put(NumberLiteral.class, NUMBER_LITERAL);
+        sConversionMap.put(CharacterLiteral.class, CHARACTER_LITERAL);
+        sConversionMap.put(InfixExpression.class, INFIX_EXPRESSION);
+        sConversionMap.put(ArrayCreation.class, ARRAY_CREATION);
         sConversionMap.put(ArrayInitializer.class, ARRAY_INITIALIZER);
-        sConversionMap.put(ArrayQualifiedTypeReference.class, QUALIFIED_TYPE);
-        sConversionMap.put(ArrayReference.class, ARRAY_ACCESS);
-        sConversionMap.put(ArrayTypeReference.class, ARRAY_TYPE);
-        sConversionMap.put(BinaryExpression.class, INFIX_EXPRESSION);
-        sConversionMap.put(CombinedBinaryExpression.class, INFIX_EXPRESSION);
+        sConversionMap.put(ArrayAccess.class, ARRAY_ACCESS);
+        sConversionMap.put(ArrayType.class, ARRAY_TYPE);
         sConversionMap.put(Block.class, BLOCK);
         sConversionMap.put(CastExpression.class, CAST_EXPRESSION);
-        sConversionMap.put(ClassLiteralAccess.class, TYPE_LITERAL);
+        sConversionMap.put(TypeLiteral.class, TYPE_LITERAL);
         sConversionMap.put(ConditionalExpression.class, CONDITIONAL_EXPRESSION);
-        sConversionMap.put(EqualExpression.class, INFIX_EXPRESSION);
-        sConversionMap.put(FieldReference.class, FIELD_ACCESS);
-        sConversionMap.put(QualifiedNameReference.class, QUALIFIED_NAME);
-        sConversionMap.put(SingleNameReference.class, SIMPLE_NAME);
-        sConversionMap.put(QualifiedSuperReference.class, QUALIFIED_NAME);
-        sConversionMap.put(SuperReference.class, SIMPLE_NAME);
-        sConversionMap.put(QualifiedThisReference.class, QUALIFIED_NAME);
-        sConversionMap.put(ThisReference.class, SIMPLE_NAME);
-        sConversionMap.put(UnaryExpression.class, PREFIX_EXPRESSION);
-        sConversionMap.put(InstanceOfExpression.class, INSTANCEOF_EXPRESSION);
+        sConversionMap.put(FieldAccess.class, FIELD_ACCESS);
+        sConversionMap.put(QualifiedName.class, QUALIFIED_NAME);
+        sConversionMap.put(SimpleName.class, SIMPLE_NAME);
+        sConversionMap.put(PrefixExpression.class, PREFIX_EXPRESSION);
+        sConversionMap.put(InstanceofExpression.class, INSTANCEOF_EXPRESSION);
         sConversionMap.put(FieldDeclaration.class, FIELD);
         sConversionMap.put(MethodDeclaration.class, METHOD);
-        sConversionMap.put(Clinit.class, METHOD);
-        sConversionMap.put(ConstructorDeclaration.class, METHOD);
         sConversionMap.put(TypeDeclaration.class, CLASS);
-        sConversionMap.put(Initializer.class, FIELD);
+        sConversionMap.put(Initializer.class, FIELD); // FIXME: not clear this should map to field
     }
 
     @Override
