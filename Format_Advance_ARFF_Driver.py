ffadd2490509969b3b27967a1cad9037e1a59b45
ashley <msashleychen@gmail.com>
2017-05-31 11:48:34 -0400

    cleaned

--- Format_Advance_ARFF_Driver.py
+++
@@ -1,9 +0,0 @@
-from Format_Advance_ARFF import *
-
-file = FormatFile()
-file.formatRelations()
-file.formatAttributesShort(defaultAttributes())
-file.formatData(getDatafromTxt())
-f = open('Weka/Data-Complete.txt', 'w')
-f.writelines(line + u'\n' for line in file.lines)
-print "done"
--- Github_API_Setup.py
+++
@@ -1,2 +0,0 @@
-from GitHub_API_Collect import *
-g = GithubOAuth('93e8006bc59db0802284441fb9cfe4b278003005')
