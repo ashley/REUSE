f13e716730390b180e70f1a06e48e036b38bccbd
ashley <msashleychen@gmail.com>
2017-06-20 16:32:14 -0400

    Created unique file r script

---
+++ Analysis/Filename_Aggregation.R
@@ -0,0 +1,32 @@
+#Import filenames first
+library(readr)
+
+getUniqueFiles <- function(filePath,filename){
+  atmosphere_fileNames <- read_delim(filePath, 
+                                     " ", escape_double = FALSE, col_names = FALSE, 
+                                     trim_ws = TRUE)
+  atmosphere <- atmosphere_fileNames
+  colnames(atmosphere)[1] <- "filename"
+  colnames(atmosphere)[2] <- "fileindex"
+  
+  #How the datafram should look
+  head(atmosphere)
+  
+  #Gets you the unique list of names in atmosphere
+  unique_files <- as.data.frame(unique(unlist(atmosphere$filename)))
+  colnames(unique_files)[1] <- "filename"
+  
+  #Compute the count of all the unique files. Might get errors if these columns already exist
+  unique_files$count <- apply(unique_files,1, function(x){length(which(grepl(x,atmosphere$filename)))})
+  unique_files$list <- apply(unique_files, 1, function(x){toString(which(grepl(x,atmosphere$filename)))})
+  
+  #Print out unique list
+  write.table(unique_files, file=paste(filename,"uniquefiles.csv",sep="_"))
+}
+
+#getUniqueFiles("/Users/ashleychen/desktop/reuse/reuse/Analysis/Filenames/atmosphere_fileNames.txt","atmosphere")
+#derby <- getUniqueFiles("/Users/ashleychen/desktop/reuse/reuse/Analysis/Filenames/derby_fileNames.txt")
+#elasticsearch <- getUniqueFiles("/Users/ashleychen/desktop/reuse/reuse/Analysis/Filenames/elasticsearch_fileNames.txt")
+#netty <- getUniqueFiles("/Users/ashleychen/desktop/reuse/reuse/Analysis/Filenames/netty_fileNames.txt")
+#openjpa <- getUniqueFiles("/Users/ashleychen/desktop/reuse/reuse/Analysis/Filenames/openjpa_fileNames.txt")
+
